50b6a6c6c5252d5cabd84a969fb44bc72b4191a2
Rajnikant17 <pandeyrajnikant.42@gmail.com>
2019-10-03 10:17:49 -0400

    First Commit

---
+++ app/src/main/java/appscrip/interview/project/Activities/MainActivity.java
@@ -0,0 +1,16 @@
+package appscrip.interview.project.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import appscrip.interview.project.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
